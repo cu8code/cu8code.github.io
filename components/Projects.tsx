01f67d6af5114ff276af7bd7cdf325d4a3e7c2a3
cu8code <git8ankanroy@gmail.com>
2024-06-25 21:30:00 +0530

    added video

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -3,6 +3,7 @@ import ScrollRevealText from "./ScrollRevelText";
 import codeRunnerPng from "@/assets/code_runner.png"
 import { FaExternalLinkAlt } from "react-icons/fa";
 
+
 const Project = () => {
     return (
         <div className="pt-40 p-2 lg:p-10 z-10 space-y-8 lg:space-y-16 m-auto max-w-7xl w-full">
@@ -18,7 +19,7 @@ const Project = () => {
                         <FaExternalLinkAlt className="font-bold max-w-4xl" />
                     </div>
                     <div className="flex flex-col lg:flex-row w-full h-full gap-5 mt-10">
-                        <video className="m-auto shadow-2xl" src="https://private-user-images.githubusercontent.com/109351887/224492244-29fdf99f-c4d1-4118-9bb4-c18524200bca.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MTkzMjI4MTYsIm5iZiI6MTcxOTMyMjUxNiwicGF0aCI6Ii8xMDkzNTE4ODcvMjI0NDkyMjQ0LTI5ZmRmOTlmLWM0ZDEtNDExOC05YmI0LWMxODUyNDIwMGJjYS5tcDQ_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNjI1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDYyNVQxMzM1MTZaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0wNmY3ZjdkZmZmOGE3ZDc3ODBlNzYxMWFlYjUyMmVhYzJiMTJiYTRmYTI4OTg5NTNhNjEyZmJlYjU5Nzk1ODUzJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.0KVBROFe1FkFt3ZPFHuwKsHR46DIauAwe_G4hCJGulQ" width={600} height={400} controls={false} autoPlay={true} loop={true} />
+                        <video width="600" height="400" controls={false} preload="none" autoPlay={true} loop={true} muted={true} src="/flang.mp4" />
                         <div className="grid place-items-center p-2 text-xl overflow-hidden">
                             I created this fun programming language to gain a better understanding of how programming languages work. its very basic , as of writing this README it can
                         </div>
@@ -26,7 +27,7 @@ const Project = () => {
                 </ScrollRevealText>
 
                 <ScrollRevealText>
-                <div className="flex flec-col gap-2 text-2xl lg:text-4xl">
+                    <div className="flex flec-col gap-2 text-2xl lg:text-4xl">
                         <a href="https://code-runner-tau.vercel.app/" className="font-bold max-w-4xl">Code Runer</a>
                         <FaExternalLinkAlt className="font-bold max-w-4xl" />
                     </div>
